f8df54f090dc07740a15ca0e6168378ce9d6cefa
jesse.greathouse <jgreathouse@balfour.com>
2015-01-20 16:34:52 -0600

    many customizations to bearded and woocommerce

--- wordpress/wp-content/plugins/woocommerce/includes/wc-template-hooks.php
+++ wordpress/wp-content/plugins/woocommerce/includes/wc-template-hooks.php
@@ -86,7 +86,7 @@ add_action( 'woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30 )
  * @see woocommerce_template_loop_price()
  * @see woocommerce_template_loop_rating()
  */
-add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
+#add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
 add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10 );
 add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
 add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5 );
@@ -143,11 +143,11 @@ add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_s
  * @see woocommerce_variable_add_to_cart()
  * @see woocommerce_external_add_to_cart()
  */
-add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
-add_action( 'woocommerce_simple_add_to_cart', 'woocommerce_simple_add_to_cart', 30 );
-add_action( 'woocommerce_grouped_add_to_cart', 'woocommerce_grouped_add_to_cart', 30 );
-add_action( 'woocommerce_variable_add_to_cart', 'woocommerce_variable_add_to_cart', 30 );
-add_action( 'woocommerce_external_add_to_cart', 'woocommerce_external_add_to_cart', 30 );
+#add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+#add_action( 'woocommerce_simple_add_to_cart', 'woocommerce_simple_add_to_cart', 30 );
+#add_action( 'woocommerce_grouped_add_to_cart', 'woocommerce_grouped_add_to_cart', 30 );
+#add_action( 'woocommerce_variable_add_to_cart', 'woocommerce_variable_add_to_cart', 30 );
+#add_action( 'woocommerce_external_add_to_cart', 'woocommerce_external_add_to_cart', 30 );
 
 /**
  * Pagination after shop loops
